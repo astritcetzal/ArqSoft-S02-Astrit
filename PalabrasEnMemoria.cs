26bef9e6afabe6f2b9979e50de68445ee0e8d243
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-05-12 12:55:53 -0600

    REFACTOR: Separar en IRepositorioPalabras y PalabrasEnMemoria

---
+++ PalabrasEnMemoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahorcado // O el nombre del namespace que estés utilizando
+{
+    public class PalabrasEnMemoria : IRepositorioPalabras
+    {
+        private readonly List<string> _palabras = new()
+        {
+            "arquitectura",
+            "interfaz",
+            "polimorfismo",
+            "encapsulamiento",
+            "herencia"
+        };
+
+        public string ObtenerPalabraAleatoria()
+        {
+            var random = new Random();
+            return _palabras[random.Next(_palabras.Count)];
+        }
+    }
+}
